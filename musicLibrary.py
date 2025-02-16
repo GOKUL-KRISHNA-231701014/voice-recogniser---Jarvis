e3a277344871bcad8bdddba75cfb7f0507c6c73c
GOKUL-KRISHNA-231701014 <231701014@rajalakshmi.edu.in>
2025-02-16 23:06:44 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "origin" : "https://youtu.be/Y7GIOnk7n3Q?si=FiNzegBE1um9ZFWB",
+    "nights" : "https://youtu.be/UtF6Jej8yb4?si=e6Zh4iz-hlho3SdT",
+    "skyfall" : "https://www.youtube.com/watch?v=DeumyOzKqgI&pp=ygUHc2t5ZmFsbA%3D%3D"
+}
